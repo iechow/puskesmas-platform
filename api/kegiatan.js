d8bd251343dc4e72f3cd6d33c0b1535258f6b1a2
iechow <93868415+iechow@users.noreply.github.com>
2026-01-30 14:05:57 +0900

    connect kegiatan api to supabase using env

--- api/kegiatan.js
+++ api/kegiatan.js
@@ -1,6 +1,33 @@
+import { createClient } from '@supabase/supabase-js'
+
 export default async function handler(req, res) {
-  res.status(200).json({
-    SUPABASE_URL: process.env.SUPABASE_URL || null,
-    SUPABASE_PUBLISHABLE_KEY: process.env.SUPABASE_PUBLISHABLE_KEY || null
-  });
+  try {
+    const supabaseUrl = process.env.https://gtbxoqzndwjlkdyweopu.supabase.co
+    const supabaseKey = process.env.sb_publishable_QEKeX4zE2L1Z7xnkxPJi1A_oGAlZJid
+
+    // cek apakah env masuk
+    if (!supabaseUrl || !supabaseKey) {
+      return res.status(500).json({
+        error: "ENV_MISSING",
+        SUPABASE_URL: supabaseUrl,
+        SUPABASE_PUBLISHABLE_KEY: supabaseKey
+      })
+    }
+
+    const supabase = createClient(supabaseUrl, supabaseKey)
+
+    const { data, error } = await supabase
+      .from('kegiatan')
+      .select('*')
+      .order('tanggal', { ascending: false })
+
+    if (error) {
+      return res.status(500).json({ error: error.message })
+    }
+
+    res.status(200).json(data)
+
+  } catch (err) {
+    res.status(500).json({ crash: err.message })
+  }
 }
