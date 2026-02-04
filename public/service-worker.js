873ba3cbf14f2ef1bad56608bda497f6145744e9
iechow <93868415+iechow@users.noreply.github.com>
2026-02-04 15:50:44 +0900

    Update service-worker.js

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,25 +1,43 @@
-const CACHE_NAME = "puskesmas-v1";
+const CACHE_NAME = "puskesmas-v2";
 
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/kegiatan.html",
-  "/laporan.html",
-  "/profil.html",
-  "/menu.html",
-  "/style.css"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+self.addEventListener("install", event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache =>
+      cache.addAll([
+        "/",
+        "/index.html",
+        "/kegiatan.html",
+        "/style.css",
+        "/menu.html"
+      ])
+    )
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(response => {
-      return response || fetch(e.request);
-    })
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(k => {
+          if (k !== CACHE_NAME) return caches.delete(k);
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request)
+      .then(response => {
+        const copy = response.clone();
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, copy);
+        });
+        return response;
+      })
+      .catch(() => caches.match(event.request))
   );
 });
