fddd3c2cfed09e15abbeab2352f22c2c059cc100
iechow <93868415+iechow@users.noreply.github.com>
2026-02-06 12:38:51 +0900

    Update jadwal.js

--- api/jadwal.js
+++ api/jadwal.js
@@ -4,7 +4,7 @@ export default async function handler(req, res) {
   try {
     const supabase = createClient(
       process.env.SUPABASE_URL,
-      process.env.SUPABASE_SERVICE_KEY
+      process.env.SUPABASE_SERVICE_ROLE_KEY
     );
 
     if (req.method === "GET") {
